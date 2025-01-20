b27cfa30f80fc20ba8d9d572c979814f418fd11f
ArturMukhamadeev <praddartur@gmail.com>
2025-01-20 19:54:09 +0300

    Final

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,146 @@
-package org.example;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    // Задание 1
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    // Задание 2
+    public static void checkSumSign() {
+        int a = 5;
+        int b = -10;
+        int sum = a + b;
+        if (sum >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
         }
+    }
+
+    // Задание 3
+    public static void printColor() {
+        int value = 50;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
+        }
+    }
+
+    // Задание 4
+    public static void compareNumbers() {
+        int a = 15;
+        int b = 10;
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+
+    // Задание 5
+    public static boolean isSumInRange(int a, int b) {
+        int sum = a + b;
+        return sum >= 10 && sum <= 20;
+    }
+
+    // Задание 6
+    public static void printNumberSign(int number) {
+        if (number >= 0) {
+            System.out.println("Число положительное");
+        } else {
+            System.out.println("Число отрицательное");
+        }
+    }
+
+    // Задание 7
+    public static boolean isNegative(int number) {
+        return number < 0;
+    }
+
+    // Задание 8
+    public static void printStringMultipleTimes(String text, int count) {
+        for (int i = 0; i < count; i++) {
+            System.out.println(text);
+        }
+    }
+
+    // Задание 9
+    public static boolean isLeapYear(int year) {
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                return year % 400 == 0;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Задание 10
+    public static void invertArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (array[i] == 0) ? 1 : 0;
+        }
+    }
+
+    // Задание 11
+    public static int[] fillArray() {
+        int[] array = new int[100];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i + 1;
+        }
+        return array;
+    }
+
+    // Задание 12
+    public static void multiplyIfLessThanSix(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < 6) {
+                array[i] *= 2;
+            }
+        }
+    }
+
+    // Задание 13
+    public static int[][] fillDiagonal(int size) {
+        int[][] matrix = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            matrix[i][i] = 1;
+        }
+        return matrix;
+    }
+
+    // Задание 14
+    public static int[] createArray(int len, int initialValue) {
+        int[] array = new int[len];
+        for (int i = 0; i < len; i++) {
+            array[i] = initialValue;
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+
+        System.out.println(isSumInRange(5, 10));
+        printNumberSign(-5);
+        System.out.println(isNegative(-5));
+        printStringMultipleTimes("Привет", 3);
+        System.out.println(isLeapYear(2020));
+
+        int[] array = {1, 1, 0, 0, 1};
+        invertArray(array);
+
+        int[] filledArray = fillArray();
+        int[] customArray = createArray(5, 42);
+        int[][] diagonalMatrix = fillDiagonal(5);
     }
 }
